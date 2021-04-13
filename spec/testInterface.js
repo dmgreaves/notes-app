725d39f1029fdbf33949c9a233e18f11cdcf4a00
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-13 14:42:31 +0100

    Added beautiful testing page

--- spec/testInterface.js
+++ spec/testInterface.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  test = new Testing
+  test = new Testing;
 // Add tests here
+//test.saveTest("Describe your test", test.?????(,))
   test.saveTest("Checking that 3 is 3", test.isEqual(3,3));
   test.saveTest("Checking that 2 is 2", test.isEqual(2,2));
   test.saveTest("Checking that 5 is not 4", test.isNotEqual(5,4));
-
+  test.saveTest("Testing if 1 is 1", test.isEqual(1,1));
 
 
 
