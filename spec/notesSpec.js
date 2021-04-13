c0422d2b386710c155344be27a06903fab8654f5
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-13 14:28:58 +0100

    Deleted redundant files

--- spec/notesSpec.js
+++
@@ -1,7 +0,0 @@
-test = new Testing
-test.isEqual('Test', 'test');
-let test1 = test.runTest("Checking that 3 is 3", test.isEqual(3,3));
-let test2 = test.runTest("Checking that 2 is 2", test.isEqual(2,2));
-test.tests.push(test1);
-test.tests.push(test2);
-test.printTests();
