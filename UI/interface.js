cac0e4b0df2ab684a22defe62b6a6fbfd324cfa0
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-14 17:08:29 +0100

    broken broken broken

--- UI/interface.js
+++ UI/interface.js
@@ -22,7 +22,7 @@ function saveNote() {
   var textInput = document.getElementById("textInput").value;
   console.log(titleInput, textInput);
   notebook.create(titleInput, textInput);
-  newupdateNotes(); //this will be moved and called when the show full note function is called
+  updateNotes(); //this will be moved and called when the show full note function is called
   hideForm();
   //listNote(textInput);
 }
@@ -37,30 +37,38 @@ function saveNote() {
 // }
 var noteList = document.getElementById("newNote");
 // helped by this: https://stackoverflow.com/questions/37796075/make-a-clickable-array-inside-javascript
-function newupdateNotes() {
-  document.getElementById("newNote").innerHTML = "";
+function updateNotes() {
+  noteList.innerHTML = "";
   notebook.notes.forEach(function(note){
     var div = document.createElement('div');
-    // was div.textContent
     div.textContent = note.title;
+    console.log(note.title);
     div.textContent += ": ";
     div.textContent += notebook.abbrev(note.text);
     div.title = note.text;
-    //div.textContent.style.visibility = "hidden";
     noteList.appendChild(div);
   });
+  console.log(noteList.innerHTML);
 }
 
 noteList.addEventListener('click', function(event){
   if (event.target !== this) {
     console.log(event.target.textContent);
     console.log(event.target.title);
+    document.getElementById("full-text").innerHTML = event.target.title;
+    }
+  });
+
+document.getElementById("return-button").addEventListener('click', function(event){
+    updateNotes();
+    //noteList.style.visibility = "show";
+    //document.getElementById("full-text").innerHTML = "Parrots have an intricate system of sounds, shrills and squawks that can tell other parrots things, from warning them of predators to telling them there's food in the vicinity.";
+});
+    //event.target.style.visibility = "hidden";
+
     // hide all the other div elements on the page (might need a container)
     // set a div container temporarily to the text of the event.target.title
     // show that div container
-
-  }
-});
 
 function abbreviate(textInput){
   //abbreviates textInput to 20 characters
