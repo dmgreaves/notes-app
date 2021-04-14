cb4e75fd846157e4829e7faa55b536c933158374
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-14 16:30:12 +0100

    started emoji API, needs fix
    Co-authored-by: flora <fg24davies@noreply.users.github.com>

--- spec/notebookSpec.js
+++ spec/notebookSpec.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
   test = new Testing;
   let notebook = new Notebook();
+  let test_note = notebook.create("Rios future!", "Rio will be a :star:")
   let errors = [];
 // Add tests here
 //test.saveTest("Describe your test", test.?????(,))
   try {
-    test.saveTest("Notebook creates a new note", test.isInstance(notebook.create("Rio will be a star and Layla will follow"), Note));
+    test.saveTest("Notebook creates a new note", test.isInstance(notebook.create("Rios future!", "Rio will be a star and Layla will follow"), Note));
     test.saveTest("New notes are saved into the notebook", test.isEqual(notebook.notes[0].text,"Rio will be a star and Layla will follow"));
     test.saveTest("Note text is abbreviated", test.isEqual(notebook.abbrev(notebook.notes[0].text),"Rio will be a sta..." ));
+    test.saveTest("converts empji shortcode to emoji", test.isEqual(test_note.text, "Rio will be a ⭐"));
+    // console.log(test_note);
+    // console.log(test_note.text);
+
   } catch (err) {
     console.log(err);
     errors.push(err);
   }
-  
+
 // Prints Tests
   let result = test.printTests();
   document.getElementById("id").innerHTML = result;
--- src/notebook.js
+++ src/notebook.js
@@ -3,15 +3,43 @@ class Notebook {
   constructor() {
      this.notes = [];
    }
-  create(text) {
-    let noteInstance = new Note(text);
-    this.notes.push(noteInstance);
-    return noteInstance;
-  }
+
 
   abbrev(text) {
     return text.slice(0,17) + "...";
   }
 
-  
+  // from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  async postData(url = '', data = {}) {
+      console.log(2)
+      const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+      });
+      console.log(4)
+      return response.json();
+    }
+
+    emojify(text) {
+        console.log(1)
+        this.postData('https://makers-emojify.herokuapp.com/', {text: text})
+            .then(data => {
+                console.log(5)
+                console.log(data); // JSON data parsed by `data.json()` call
+                console.log(data.emojified_text);
+                return data.emojified_text;
+
+            });
+
+        console.log(3)
+    }
+
 }
